f2f1d7ff61dee2ff9449bf95d45fd1aa25c571bf
ericmita <eric.mita.3@gmail.com>
2023-01-23 16:49:29 +0200

    upload document

--- src/screens/documentDetails/components/documentAttachments/index.tsx
+++ src/screens/documentDetails/components/documentAttachments/index.tsx
@@ -1,24 +1,74 @@
-import Grid from "@mui/material/Grid";
-import {Button, Card, CardContent, Chip, Stack} from "@mui/material";
-import React from "react";
+import Grid from '@mui/material/Grid';
+import { Button, Card, CardContent, Chip, Stack, Typography } from '@mui/material';
+import React, { useState, ChangeEvent, createRef } from 'react';
 
 function DocumentAttachments() {
-    return (
-        <Grid container spacing={4}>
-            <Grid item md={10}>
-                <Card>
-                    <CardContent>
-                        <Chip label="Atasamente" color="primary" size="medium" sx={{mb: 3}}/>
-                    </CardContent>
-                </Card>
-            </Grid>
+  const [document, setDocument] = useState<File | null>(null);
+  const [uploading, setUploading] = useState<boolean>(false);
+  const documentInput = createRef<HTMLInputElement>();
 
-            <Grid item md={2}>
-                <Stack gap={4}>
-                    <Button variant='contained'>Ataseaza document</Button>
-                </Stack>
-            </Grid>
-        </Grid>);
+  const handleFileSelect = async (e: ChangeEvent<HTMLInputElement>) => {
+    if (documentInput.current) {
+      const inputFile = documentInput.current;
+      if (inputFile.files?.[0]) setDocument(inputFile.files?.[0]);
+      if (e.target.files?.[0]) {
+        setUploading(true);
+        const formData = new FormData();
+        formData.append('file', document as File);
+        try {
+          // TODO: replace dummy endpoint with real one
+          const response = await fetch('https://httpbin.org/post', {
+            method: 'POST',
+            body: formData,
+            headers: {
+              'Content-Type': 'multipart/form-data',
+            },
+          });
+          if (!response.ok) {
+            throw new Error(response.statusText);
+          }
+          console.log('Document atasat!');
+        } catch (error) {
+          console.error('Eroare in atasarea documentului: ', error);
+        }
+
+        setUploading(false);
+      }
+    }
+  };
+  return (
+    <Grid container spacing={4}>
+      <Grid item md={10}>
+        <Card>
+          <CardContent>
+            <Chip label='Atasamente' color='primary' size='medium' sx={{ mb: 3 }} />
+            <Typography variant='h5' sx={{ mb: 3 }}>
+              {document && document.name}
+            </Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+
+      <Grid item md={2}>
+        <Stack gap={4}>
+          <input
+            type='file'
+            onChange={handleFileSelect}
+            style={{ display: 'none' }}
+            ref={documentInput}
+          />
+          <Button
+            variant='contained'
+            type='button'
+            onClick={() => documentInput.current?.click()}
+            disabled={uploading}
+          >
+            Ataseaza
+          </Button>
+        </Stack>
+      </Grid>
+    </Grid>
+  );
 }
 
 export default React.memo(DocumentAttachments);
