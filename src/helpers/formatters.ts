778f112023fcaa593dbc956591239a55c97ccb8b
ericmita <eric.mita.3@gmail.com>
2023-02-03 09:45:15 +0200

    overdue documents

--- src/helpers/formatters.ts
+++ src/helpers/formatters.ts
@@ -1,3 +1,10 @@
 export const capitalizeString = (str: string | undefined) => {
   return str ? str.charAt(0).toUpperCase() + str.slice(1) : null;
 };
+
+export const isDateOverdue = (due: Date | undefined) => {
+  const dueDate = due ? new Date(due) : undefined;
+  const currentDate = new Date();
+
+  return dueDate ? dueDate < currentDate : undefined;
+};
