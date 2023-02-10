432431c04c732141190b273301239efb652626aa
eric-mita <36814085+eric-mita@users.noreply.github.com>
2023-02-10 14:25:19 +0200

    overdue documents (#37)
    * overdue documents
    
    * addresing PR comment
    
    * improving overdueDate checker
    
    * improving overdueDate checker
    
    * removing wrong file
    
    ---------
    
    Co-authored-by: ericmita <eric.mita.3@gmail.com>

--- src/components/documentsTableDocumentMarks/index.tsx
+++ src/components/documentsTableDocumentMarks/index.tsx
@@ -1,21 +1,39 @@
 import React from 'react';
-import styled from "styled-components";
-import {Box, Tooltip} from "@mui/material";
+import styled from 'styled-components';
+import { Box, Tooltip } from '@mui/material';
 import StarsIcon from '@mui/icons-material/Stars';
 import WarningIcon from '@mui/icons-material/Warning';
-import {DocumentDto} from "services/api/dtos";
+import { DocumentDto } from 'services/api/dtos';
+import EventBusyIcon from '@mui/icons-material/EventBusy';
+import { pink } from '@mui/material/colors';
+import { isDateOverdue } from 'helpers/formatters';
 
 const StyledBox = styled(Box)`
   display: flex;
 `;
 
 export interface DocumentMarksProps {
-    document: DocumentDto;
+  document: DocumentDto;
 }
 
 export const DocumentMarks = ({ document }: DocumentMarksProps) => {
-    return <StyledBox>
-        {document.project?.presentsInterest && <Tooltip title='Proiect de interes/cu impact' placement='top'><StarsIcon color="primary" fontSize="small"/></Tooltip>}
-        {document.isRulesBreaker && <Tooltip title='Document ce contravine normelor in vigoare' placement='top'><WarningIcon color="warning" fontSize="small"/></Tooltip>}
+  return (
+    <StyledBox>
+      {document.project?.presentsInterest && (
+        <Tooltip title='Proiect de interes/cu impact' placement='top'>
+          <StarsIcon color='primary' fontSize='small' />
+        </Tooltip>
+      )}
+      {document.isRulesBreaker && (
+        <Tooltip title='Document ce contravine normelor in vigoare' placement='top'>
+          <WarningIcon color='warning' fontSize='small' />
+        </Tooltip>
+      )}
+      {isDateOverdue(document.deadline) && (
+        <Tooltip title='Data limita atinsa' placement='top'>
+          <EventBusyIcon sx={{ color: pink[400] }} fontSize='small' />
+        </Tooltip>
+      )}
     </StyledBox>
-}
+  );
+};
--- src/helpers/formatters.ts
+++ src/helpers/formatters.ts
@@ -1,3 +1,10 @@
 export const capitalizeString = (str: string | undefined) => {
   return str ? str.charAt(0).toUpperCase() + str.slice(1) : null;
 };
+
+export const isDateOverdue = (due: Date | undefined) => {
+  if(due === undefined) return false;
+  const dueDate = new Date(due);
+  const currentDate = new Date();
+  return dueDate < currentDate;
+};
