210496813dbffcb8fa32474330aa5d5f486c6ba6
Catalin Stoian <stoian.ioan.catalin@gmail.com>
2023-03-17 10:57:40 +0200

    add new sources

--- src/constants/translations.ts
+++ src/constants/translations.ts
@@ -1,5 +1,10 @@
 export const Translations: Record<string, string> = {
     "senat": "Senat",
     "guvern": "Guvern",
-    "camera_deputatilor": "Camera Deputatilor"
+    'camera_deputatilor': 'Camera Deputaților',
+    'mfinante': 'Ministerul Finanțelor',
+    'mmediu': 'Ministerul Mediului',
+    'mdezvoltarii': 'Ministerul Dezvoltării',
+    'meducatiei': 'Ministerul Educației',
+    'mtransport': 'Ministerul Transporturilor',
 }
