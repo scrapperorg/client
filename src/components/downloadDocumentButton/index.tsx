ad2487fbd40bb7729f21ef6f448150c2e79f208d
ericmita <eric.mita.3@gmail.com>
2023-03-22 14:42:45 +0200

    using the pdfviewurl to download docs

--- src/components/downloadDocumentButton/index.tsx
+++ src/components/downloadDocumentButton/index.tsx
@@ -2,15 +2,18 @@ import React from 'react';
 import DownloadIcon from '@mui/icons-material/Download';
 import { IconButton } from '@mui/material';
 import styled from 'styled-components';
+import config from 'config/index';
 
 interface DownloadDocumentButtonProps {
   fileUrl: string;
 }
 
 const DownloadDocumentButton: React.FC<DownloadDocumentButtonProps> = ({ fileUrl }) => {
+  const pdfViewerUrl = `${config.BASE_URL}document/download-highlight-pdf/${fileUrl}`;
+
   return (
     <IconButton>
-      <StyledAnchor href={fileUrl} download>
+      <StyledAnchor href={pdfViewerUrl} download>
         <DownloadIcon fontSize='small' />
       </StyledAnchor>
     </IconButton>
