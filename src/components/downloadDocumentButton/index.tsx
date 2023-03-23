04a8793bd6ae53fb2dcb4ffc7cc5e259f7041371
eric-mita <36814085+eric-mita@users.noreply.github.com>
2023-03-23 14:11:58 +0700

    using the pdfviewurl to download docs (#59)
    * using the pdfviewurl to download docs
    
    * using new download pdf route
    
    * checking if file type is pdf
    
    ---------
    
    Co-authored-by: ericmita <eric.mita.3@gmail.com>

--- src/components/downloadDocumentButton/index.tsx
+++ src/components/downloadDocumentButton/index.tsx
@@ -2,20 +2,44 @@ import React from 'react';
 import DownloadIcon from '@mui/icons-material/Download';
 import { IconButton } from '@mui/material';
 import styled from 'styled-components';
+import config from 'config/index';
 
 interface DownloadDocumentButtonProps {
   fileUrl: string;
 }
 
 const DownloadDocumentButton: React.FC<DownloadDocumentButtonProps> = ({ fileUrl }) => {
+  const rawPdfDownloaderUrl = `${config.BASE_URL}document/download-pdf/${fileUrl}`;
+
+  const handleDownload = () => {
+    fetch(rawPdfDownloaderUrl)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`Failed to download file: ${response.status} ${response.statusText}`);
+        }
+        const contentType = response.headers.get('content-type');
+        if (!contentType || !contentType.startsWith('application/pdf')) {
+          throw new Error('Invalid file format. Only PDF files are supported.');
+        }
+        response.blob().then(blob => {
+          const url = window.URL.createObjectURL(blob);
+          const a = document.createElement('a');
+          a.href = url;
+          a.download = rawPdfDownloaderUrl.substring(rawPdfDownloaderUrl.lastIndexOf('/') + 1);
+          a.click();
+        });
+      }).catch(error => {
+        console.error(error);
+      });
+  };
+
   return (
-    <IconButton>
-      <StyledAnchor href={fileUrl} download>
-        <DownloadIcon fontSize='small' />
-      </StyledAnchor>
+    <IconButton onClick={handleDownload}>
+      <DownloadIcon fontSize='small' />
     </IconButton>
   );
 };
+
 
 export const StyledAnchor = styled.a`
   text-decoration: none;
