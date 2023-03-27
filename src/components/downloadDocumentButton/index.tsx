e5efc39614531518ffabc8243e9050b627fba97f
ericmita <eric.mita.3@gmail.com>
2023-03-27 10:59:11 +0300

    code cleanup

--- src/components/downloadDocumentButton/index.tsx
+++ src/components/downloadDocumentButton/index.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import DownloadIcon from '@mui/icons-material/Download';
 import { IconButton } from '@mui/material';
 import config from 'config/index';
-import { downloadBlob } from 'helpers/downloadBlob';
-// import { documentApiService } from "services/api/DocumentApiService";
 import { useDocuments } from 'screens/monitor/hooks/useDocuments';
 
 interface DownloadDocumentButtonProps {
@@ -16,8 +14,6 @@ const DownloadDocumentButton: React.FC<DownloadDocumentButtonProps> = ({ fileUrl
   const { downloadRawPdf } = useDocuments();
 
   const handleDownload = () => {
-    console.log('componenta')
-
     downloadRawPdf(rawPdfDownloaderUrl);
   };
 
