9b7cd602c6467d66ba15839416f794b0819b00fd
Lucian Barticel <mail@lucianbarticel.ro>
2023-04-20 09:59:35 +0300

    fix lint issue

--- src/screens/usersManagement/hooks/useModal.ts
+++ src/screens/usersManagement/hooks/useModal.ts
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import { useContext } from 'react';
 import { InteractiveComponentsContext } from 'contexts/interactiveComponentsContext';
 
 export function useModal(currentModal: string) {
