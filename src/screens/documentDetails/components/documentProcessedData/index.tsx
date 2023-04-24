5c8ef9b0f8091701566bb97b663ca338a25ff1ae
eric-mita <36814085+eric-mita@users.noreply.github.com>
2023-04-24 13:17:25 +0700

    populating fields with real data and created new progress component (#80)
    Co-authored-by: ericmita <eric.mita.3@gmail.com>

--- src/screens/documentDetails/components/documentProcessedData/index.tsx
+++ src/screens/documentDetails/components/documentProcessedData/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import Grid from '@mui/material/Grid';
 import { Button, Card, CardContent, Chip, Stack, Typography } from '@mui/material';
+import { DocumentDto } from 'services/api/dtos';
+import { Translations } from 'constants/translations';
+import CircularProgressIndicator from 'components/circularProgressIndicator';
 
 interface DocumentProcessedDataProps {
   onDownloadOcrPdf: () => void;
+  document: DocumentDto;
 }
 
 function DocumentProcessedData(props: DocumentProcessedDataProps) {
-  const { onDownloadOcrPdf } = props;
+  const { onDownloadOcrPdf, document } = props;
   return (
     <Grid container spacing={4}>
       <Grid item md={10}>
@@ -17,30 +21,36 @@ function DocumentProcessedData(props: DocumentProcessedDataProps) {
             <Grid container spacing={4}>
               <Grid item md={4}>
                 <Typography variant='h4' sx={{ mb: 3 }}>
+                  Status OCR-izare:
+                </Typography>
+                <Typography variant='h4' sx={{ mb: 3 }}>
                   Format original:
                 </Typography>
                 <Typography variant='h4' sx={{ mb: 3 }}>
                   Nr de pagini:
                 </Typography>
                 <Typography variant='h4' sx={{ mb: 3 }}>
-                  Articole Identificate:
-                </Typography>
-                <Typography variant='h4' sx={{ mb: 3 }}>
                   Termeni identificati:
                 </Typography>
+                <Typography variant='h4' sx={{ mb: 3 }}>
+                  Precizie interpretare text:
+                </Typography>
               </Grid>
-              <Grid item md={4}>
+              <Grid item md={6}>
                 <Typography variant='h5' sx={{ mb: 3 }}>
-                  Not yet implemented
+                  {Translations[document.processingStatus] || 'N/A'}
                 </Typography>
                 <Typography variant='h5' sx={{ mb: 3 }}>
-                  Not yet implemented
+                  {document.link.split('.').pop()?.toUpperCase() || 'N/A'}
                 </Typography>
                 <Typography variant='h5' sx={{ mb: 3 }}>
-                  Not yet implemented
+                  {document.numberOfPages || 'N/A'}
+                </Typography>
+                <Typography variant='h5' sx={{ mb: 2 }}>
+                  {document.numberOfIdentifiedTerms || 'N/A'}
                 </Typography>
                 <Typography variant='h5' sx={{ mb: 3 }}>
-                  Not yet implemented
+                  <CircularProgressIndicator percentage={document.textInterpretationPrecision || 0}/>
                 </Typography>
               </Grid>
               <Grid item md={4}></Grid>
