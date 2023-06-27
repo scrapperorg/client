03c7f5671e366dd7936b5bdd62f089d6c16c9072
silviud32 <117361301+silviud32@users.noreply.github.com>
2023-06-27 08:59:37 +0300

    add warning modal before creating a keyword (#116)
    * add warning modal before creating a keyword
    
    * Update src/screens/usersManagement/hooks/useModal.ts

--- src/screens/usersManagement/hooks/useModal.ts
+++ src/screens/usersManagement/hooks/useModal.ts
@@ -2,18 +2,21 @@ import { useContext } from 'react';
 import { InteractiveComponentsContext } from 'contexts/interactiveComponentsContext';
 
 export function useModal(currentModal: string) {
-  const { openModal: openGenericModal, closeModal, modalName } = useContext(InteractiveComponentsContext);
-  
+  const {
+    openModal: openGenericModal,
+    closeModal,
+    modalName,
+  } = useContext(InteractiveComponentsContext);
+
   const isModalOpen = modalName === currentModal;
 
   const openModal = () => {
     openGenericModal(currentModal);
-  }
-  
+  };
+
   return {
     isModalOpen,
     openModal,
-    closeModal
-  }
-
+    closeModal,
+  };
 }
