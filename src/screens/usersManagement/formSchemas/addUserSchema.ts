6d4b421cbe93f1cae489204d894cd6db6c27f2a4
ericmita <eric.mita.3@gmail.com>
2023-07-10 13:44:48 +0300

    translated errors

--- src/screens/usersManagement/formSchemas/addUserSchema.ts
+++ src/screens/usersManagement/formSchemas/addUserSchema.ts
@@ -1,10 +1,39 @@
-import joi from "joi";
+import joi from 'joi';
 
 export const addUserSchema = joi.object({
-    name: joi.string().required().messages({'string.empty': 'Camp obligatoriu! Trebuie sa introduceti prenumele.'}),
-    surname: joi.string().required().messages({'string.empty': 'Camp obligatoriu! Trebuie sa introduceti numele.'}),
-    role: joi.string().required().messages({'string.empty': 'Camp obligatoriu! Trebuie sa selectati un rol.'}),
-    email: joi.string().email({ tlds: { allow: false } }).required().messages({'string.empty': 'Camp obligatoriu! Trebuie sa introduceti un email.'}),
-    password: joi.string().min(6).max(20).required().messages({'string.empty': 'Camp obligatoriu! Trebuie sa introduceti o parola.'}),
-    confirmPassword: joi.any().valid(joi.ref('password')).required().messages({ 'any.only': 'Parolele nu coincid' })
+  name: joi
+    .string()
+    .required()
+    .messages({ 'string.empty': 'Camp obligatoriu! Trebuie sa introduceti prenumele.' }),
+  surname: joi
+    .string()
+    .required()
+    .messages({ 'string.empty': 'Camp obligatoriu! Trebuie sa introduceti numele.' }),
+  role: joi
+    .string()
+    .required()
+    .messages({ 'string.empty': 'Camp obligatoriu! Trebuie sa selectati un rol.' }),
+  email: joi
+    .string()
+    .email({ tlds: { allow: false } })
+    .required()
+    .messages({
+      'string.empty': 'Camp obligatoriu! Trebuie sa introduceti un email.',
+      'string.email':
+        'Adresa de email invalida! Adresa trebuie sa fie de forma exemplu@exemplu.com.',
+    }),
+  password: joi
+    .string()
+    .min(6)
+    .max(20)
+    .required()
+    .messages({
+      'string.empty': 'Camp obligatoriu! Trebuie sa introduceti o parola.',
+      'string.min': 'Parola trebuie sa aiba minim 6 caractere.',
+    }),
+  confirmPassword: joi
+    .any()
+    .valid(joi.ref('password'))
+    .required()
+    .messages({ 'any.only': 'Parolele nu coincid' }),
 });
--- src/screens/usersManagement/formSchemas/changePasswordSchema.ts
+++ src/screens/usersManagement/formSchemas/changePasswordSchema.ts
@@ -1,6 +1,18 @@
-import joi from "joi";
+import joi from 'joi';
 
 export const changePasswordSchema = joi.object({
-    password: joi.string().min(6).max(20).required().messages({'string.empty': 'Camp obligatoriu! Trebuie sa introduceti o parola.'}),
-    confirmPassword: joi.any().valid(joi.ref('password')).required().messages({ 'any.only': 'Parolele nu coincid' })
+  password: joi
+    .string()
+    .min(6)
+    .max(20)
+    .required()
+    .messages({
+      'string.empty': 'Camp obligatoriu! Trebuie sa introduceti o parola.',
+      'string.min': 'Parola trebuie sa aiba minim 6 caractere.',
+    }),
+  confirmPassword: joi
+    .any()
+    .valid(joi.ref('password'))
+    .required()
+    .messages({ 'any.only': 'Parolele nu coincid' }),
 });
