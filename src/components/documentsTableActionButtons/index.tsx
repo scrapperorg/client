d9fff29788445b7dc2bbc660b93eaab325c1a4e0
ericmita <eric.mita.3@gmail.com>
2023-07-31 15:01:39 +0300

    undo the new icon

--- src/components/documentsTableActionButtons/index.tsx
+++ src/components/documentsTableActionButtons/index.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import styled from 'styled-components';
 import ViewDocumentButton from 'components/viewDocumentButton';
 import DownloadDocumentButton from 'components/downloadDocumentButton';
-import QuickAnalysisButton from 'components/quickAnalysisButton';
 
 const StyledBox = styled(Box)`
   display: flex;
@@ -14,7 +13,6 @@ export const ActionButtons = ({ document }: any) => {
     <StyledBox>
       <DownloadDocumentButton fileUrl={document.id} />
       <ViewDocumentButton fileUrl={document.link} />
-      <QuickAnalysisButton />
     </StyledBox>
   );
 };
